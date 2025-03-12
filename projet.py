a1faf015963e523ac81ea949cf2e91477e32d585
chahama2 <aminechahem@gmail.com>
2025-03-13 00:20:01 +0100

    Squash merge projeth2 into master

--- projet.py
+++ projet.py
@@ -1,11 +1,9 @@
-
-def somme(t):
-	s=0
-	for i in t:
-		s+=t
-		return s
-
 Data=[1,2,3]
+if Data:
+	print("la somme est :",sum(Data))
+	print("le min est :",min(Data))
+	print("le max est :",max(Data))
+else:
+	print("dossier vide")
 
-print("la somme est :",sum(Data)
-
+XXXXXXX
